d7387368ac81684a4eacf16d43554d7f7132db1f
anthony karabian <anthony.karabian@microdrones.com>
2021-05-27 18:53:16 -0400

    Labo 1 simulation

---
+++ src/simulation/XMLParserTest.java
@@ -0,0 +1,69 @@
+package simulation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import java.io.File;
+import java.nio.file.Path;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class XMLParserTest {
+
+    private static final String CONFIG_FILE_PATH = "/Users/anthonykarabian/Desktop/ETS/LOG121/Log121 Labo1/src/ressources/configuration.xml";
+    private static final String CONFIG_FILE = "configuration.xml";
+
+    private File selectedFile;
+
+    private XMLParser xmlParser;
+
+    private Path configPath;
+
+    @BeforeEach
+    public void setup(@TempDir Path tempDir) {
+
+            configPath = tempDir.resolve(CONFIG_FILE);
+
+            xmlParser = new XMLParser(configPath.toFile());
+    }
+
+    @Test
+    public void testInputFile(){
+
+        String expectedFileInput = configPath.toString();
+
+        assertEquals(expectedFileInput, xmlParser.getInputFile());
+    }
+
+    @Test
+    public void deserializeFile(){
+
+        xmlParser.parseFile();
+    }
+
+    public void testJbRainsbergerUsingEntireDocument()
+            throws Exception {
+
+        String expectedXmlDocument =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                        + "<configuration>"
+                        + "<metadonnees>"
+                        + "<usine type=\"usine-matiere\">"
+                        + "<icones>"
+                        + "<icone type=\"vide\" path=\"src/ressources/UMP0%.png\"/>"
+                        + "<icone type=\"un-tiers\" path=\"src/ressources/UMP33%.png\"/>"
+                        + "<icone type=\"deux-tiers\" path=\"src/ressources/UMP66%.png\"/>"
+                        + "<icone type=\"plein\" path=\"src/ressources/UMP100%.png\"/>"
+                        + "</icones>"
+                        + "<sorite type = \"metal\" />"
+                        + "<interval-production>100</interval-production>"
+                        + "</usine>"
+                        + "</metadonnees>"
+                        + "<simulation>"
+                        + "</simulation>"
+                        + "</configuration>";
+
+    }
+
+}
